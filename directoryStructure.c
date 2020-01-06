64bac7a6ea44e9d17e78e8d7ac84309f78eb4960
chris.kehl <chris_kehl@att.net>
2020-01-06 09:39:03 -0500

    first commit

---
+++ directoryStructure.c
@@ -0,0 +1,11 @@
+// making the calendar
+#include <stdio.h>
+
+main()
+{
+    // inserting a backslash into your text
+    printf("c:\\cygwin\\home\\administrator\n");
+   
+
+}
+
---
+++ janCal.c
@@ -0,0 +1,16 @@
+// making the calendar
+#include <stdio.h>
+
+main()
+{
+printf("\n");
+printf("\t\tJanuary 2020");
+printf("\n");
+printf("\nSun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+printf("\t\t\t1\t2\t3\t4\n");
+printf("5\t6\t7\t8\t9\t10\t11\n");
+printf("12\t13\t14\t15\t16\t17\t18\n");
+printf("19\t20\t21\t22\t23\t24\t25\n");
+printf("26\t27\t28\t29\t30\t31\n");
+
+}
